31e28ed0d765949a0e5cf479c4884fd996fe0eb4
FEvelychko <maxdevelopenko@gmail.com>
2015-09-25 23:58:45 +0300

    starting checking the clicked event

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,11 @@
  * Created by Maksym on 25.09.2015.
  */
 
+    function hideCell(value){
+        value.style.color = "red";
+    }
     var cells = document.getElementsByTagName('td');
     for(var i = 0; i < cells.length; i++){
         cells[i].innerHTML = Math.floor((Math.random() * 100) + 1);
+        cells[i] = hideCell(cells[i]);GIT
     }
