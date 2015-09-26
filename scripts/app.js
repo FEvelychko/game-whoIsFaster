4cb6028f3a044daad8a6335ef00752bc3f33d3e2
FEvelychko <maxdevelopenko@gmail.com>
2015-09-26 16:05:40 +0300

    updating cycle of game

--- scripts/app.js
+++ scripts/app.js
@@ -1,51 +1,70 @@
-/**
- * Created by Maksym on 25.09.2015.
- */
+    /**
+     * Created by Maksym on 26.09.2015.
+     */
 
-    var startTime;
-    var cellsLeft;
-    function changeActiveHiddenState(hiddenVal) {
+    var startTime, cellsLeft;
+
+    function changeActiveHiddenState(hiddenVal){
+
         var active = document.getElementsByClassName('active');
         for (var i = 0; i < active.length; i++) {
             active[i].hidden = hiddenVal;
         }
+
     }
-    function prepareGame() {
+
+    function prepareGame(){
+
         document.getElementById('gameover').hidden = 'hidden';
         changeActiveHiddenState('');
         var cells = document.getElementsByTagName('td');
-        for (var i = 0; i < cells.length; i++) {
-            cells[i].hidden = '';
-            cells[i].innerHTML = Math.floor((Math.random() * 100) + 1);
-            cells[i].onclick = handleClick;
+        for(var i = 0; i < cells.length; i++){
+            cells[i].hidden = "";
+            cells[i].innerHTML = Math.floor((Math.random() * 100) + 1); // set the random numbers  //
+            cells[i].onclick = handleClick;                                // onclick do hiding //
         }
         cellsLeft = 16;
         startTime = Date.now();
+
     }
-    function handleClick() {
-        //debugger;
-        var current = parseInt(this.innerHTML);
+
+    function handleClick(){
+
         var incorrect = false;
+        var current = parseInt(this.innerHTML);
         var cells = document.getElementsByTagName('td');
-        for (var i = 0; i < cells.length; i++) {
-            var cell = cells[i];
-            if (!cell.hidden) {
-                if (parseInt(cell.innerHTML) < current) {
-                    incorrect = true;
+        for(var j = 0; j < cells.length; j++){
+            var cell = cells[j];
+            if(cell.hidden == false){       // or (!cell.hidden) - is the same thing  //
+                if(parseInt(cell.innerHTML) < current){
+                    //return false;
+                    incorrect = true;       //  then do nothing and same as return false //
                 }
             }
         }
-        if (!incorrect) {
+
+        // if(true){                        // in any case do all body scripts that inside if(true) //
+        if(!incorrect) {
             this.hidden = 'hidden';
             cellsLeft--;
-            if (cellsLeft == 0) {
+            if(cellsLeft == 0){
                 finishGame();
             }
-
         }
     }
-    function finishGame() {
+
+    function finishGame(){
+
         changeActiveHiddenState('hidden');
         document.getElementById('time').innerHTML = (Date.now() - startTime) / 1000;
         document.getElementById('gameover').hidden = '';
+
     }
+
+
+
+
+
+
+
+
