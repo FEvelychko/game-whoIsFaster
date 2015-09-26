59b7c879825bba345cea8179cd20c38ab1b71ce0
FEvelychko <maxdevelopenko@gmail.com>
2015-09-26 12:55:44 +0300

    the hiding of chosen elements

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,72 @@
  * Created by Maksym on 25.09.2015.
  */
 
-    function hideCell(value){
-        value.style.color = "red";
+
+    function prepareGame(){
+
+        var cells = document.getElementsByTagName('td');
+        for(var i = 0; i < cells.length; i++){
+            cells[i].innerHTML = Math.floor((Math.random() * 100) + 1);
+            cells[i].onclick = hideCell;
+        }
     }
-    var cells = document.getElementsByTagName('td');
-    for(var i = 0; i < cells.length; i++){
-        cells[i].innerHTML = Math.floor((Math.random() * 100) + 1);
-        cells[i] = hideCell(cells[i]);GIT
+
+    function hideCell(){
+        debugger;
+        var current = parseInt(this.innerHTML);
+        cells = document.getElementsByTagName('td');
+        for(var j = 0; j < cells.length; j++){
+            if(cells[j].innerHTML == current){
+                cells[j].hidden = 'hidden';
+            }
+        }
     }
+
+
+    /*var startTime;
+    var cellsLeft;
+    function changeActiveHiddenState(hiddenVal) {
+        var active = document.getElementsByClassName('active');
+        for (var i = 0; i < active.length; i++) {
+            active[i].hidden = hiddenVal;
+        }
+    }
+    function prepareGame() {
+        document.getElementById('gameover').hidden = 'hidden';
+        changeActiveHiddenState('');
+        var cells = document.getElementsByTagName('td');
+        for (var i = 0; i < cells.length; i++) {
+            cells[i].hidden = '';
+            cells[i].innerHTML = Math.floor((Math.random() * 100) + 1);
+            cells[i].onclick = handleClick;
+        }
+        cellsLeft = 16;
+        startTime = Date.now();
+    }
+    function handleClick() {
+        debugger;
+        var current = parseInt(this.innerHTML);
+        var incorrect = false;
+        var cells = document.getElementsByTagName('td');
+        for (var i = 0; i < cells.length; i++) {
+            var cell = cells[i];
+            if (!cell.hidden) {
+                if (parseInt(cell.innerHTML) < current) {
+                    incorrect = true;
+                }
+            }
+        }
+        if (!incorrect) {
+            this.hidden = 'hidden';
+            cellsLeft--;
+            if (cellsLeft == 0) {
+                finishGame();
+            }
+
+        }
+    }
+    function finishGame() {
+        changeActiveHiddenState('hidden');
+        document.getElementById('time').innerHTML = (Date.now() - startTime) / 1000;
+        document.getElementById('gameover').hidden = '';
+    }*/
